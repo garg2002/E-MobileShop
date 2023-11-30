4f2f877bbffd9de88ab7c49d29f2a7605e6ef7bf
Satyam Garg <satyamgarg1308@gmail.com>
2023-11-30 21:19:00 +0530

    Login & Register Small device UI changes

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -69,11 +69,11 @@ export default function Register() {
 
   return (
     <div className="bg-white relative">
-      <div className="flex flex-col items-center justify-between pt-0 pr-10 pb-0 pl-10 mt-8 mr-auto xl:px-5 lg:flex-row">
-        <div className="flex flex-col justify-center items-center w-full pr-10 pl-10 lg:flex-row">
+      <div className="flex flex-col items-center justify-between pt-0 lg:pr-10 pb-0 lg:pl-10 mt-8 mr-auto xl:px-5 lg:flex-row">
+        <div className="flex flex-col justify-center items-center w-full lg:pr-10 lg:pl-10 lg:flex-row">
           <div className="w-full mt-10 mr-0 mb-0 ml-0 relative max-w-2xl lg:mt-0 lg:w-5/12">
             <div className="flex flex-col items-center justify-start pt-10 pr-10 pb-10 pl-10 bg-white shadow-2xl rounded-xl relative z-10">
-              <p className="w-full text-4xl font-medium text-center font-serif">
+              <p className="w-full text-2xl lg:text-4xl font-semibold text-center font-serif">
                 {isRegistered
                   ? "Registration Successfull !"
                   : "Sign up for an account"}
